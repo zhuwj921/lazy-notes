5be26e8ecfb1e0e888dc1a5fbaf0d010662dc7c8
zhuwj <774623096@qq.com>
2020-05-13 23:14:20 +0800

    增加业务异常 补充readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # lazy-notes
-记录通用的功能代码 ，形成自己的笔记
+> 记录功能代码 ，作为代码笔记本
+
+#公共模块
+> 公用的返回结果，异常，错误码，工具类等
+
+***
+1. 错误码参考阿里错误码
+
+#数据字典
+> 项目中可动态配置数据功能
+
+***
+
+##技术栈
+1. SpringBoot
+2. Mybatis-Plus
+3. mysql
+4. lombok
+
+***
+
+> 使用spring-boot-starter-cache，基于默认的缓存处理，可以添加
+> encache,redis等，数据库sql在SQL文件夹中: dict.sql
--- SQL/dict.sql
+++ SQL/dict.sql
@@ -1,18 +1,5 @@
-/*
-SQLyog Ultimate v11.27 (32 bit)
-MySQL - 5.7.27-log : Database - lazy_notes
-*********************************************************************
-*/
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`lazy_notes` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE `lazy_notes`;
 
 USE `lazy_notes`;
 
@@ -51,7 +38,4 @@ CREATE TABLE `dict_detail` (
 
 insert  into `dict_detail`(`id`,`deleted`,`dict_id`,`key`,`value`,`label`,`sort`) values (1,0,1,'success','1','成功',1),(2,0,1,'fail','2','失败',2);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
