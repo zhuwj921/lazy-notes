1ab42152d27ee2e4b7c778a9bffff57526bf3e3c
zhuwj <774623096@qq.com>
2020-05-14 23:07:05 +0800

    增加权限管理模块

---
+++ security/src/main/java/com/zhuwj/security/Application.java
@@ -0,0 +1,16 @@
+package com.zhuwj.security;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.zhuwj.security.mapper")
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
