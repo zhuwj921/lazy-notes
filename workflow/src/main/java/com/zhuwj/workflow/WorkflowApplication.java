31bd4b456087941ad7a3111cf75cc9e8082ab4ec
zhuwj <774623096@qq.com>
2020-05-18 18:01:12 +0800

    工作流开发

--- workflow/src/main/java/com/zhuwj/workflow/WorkflowApplication.java
+++ workflow/src/main/java/com/zhuwj/workflow/WorkflowApplication.java
@@ -1,9 +1,10 @@
 package com.zhuwj.workflow;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WorkflowApplication {
 
     public static void main(String[] args) {
