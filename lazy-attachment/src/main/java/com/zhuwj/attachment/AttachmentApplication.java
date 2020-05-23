bdbb94655723a1fea9b0c27fbb8bb90b25bf2860
zhuwj <774623096@qq.com>
2020-05-24 00:59:52 +0800

    模块优化

---
+++ lazy-attachment/src/main/java/com/zhuwj/attachment/AttachmentApplication.java
@@ -0,0 +1,17 @@
+package com.zhuwj.attachment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 附件启动类
+ * @author: zhuwj
+ * @create: 2020-05-23 20:24
+ **/
+@SpringBootApplication
+public class AttachmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AttachmentApplication.class, args);
+    }
+
+}
---
+++ lazy-common/src/main/java/com/zhuwj/common/base/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.zhuwj.common.base;
+
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
+
+/**
+ * @description: 基础实体类信息
+ * @author: zhuwj
+ * @create: 2020-05-16 23:39
+ **/
+@Data
+public class BaseEntity<T> extends Model<BaseEntity<T>> {
+
+
+
+
+}
