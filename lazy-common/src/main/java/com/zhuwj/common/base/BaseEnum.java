fdb82400f7873ef4f6d6e3a39e3723aeb5ed7d9d
zhuwj <774623096@qq.com>
2020-07-12 18:12:11 +0800

    代码优化

---
+++ lazy-common/src/main/java/com/zhuwj/common/base/BaseEnum.java
@@ -0,0 +1,20 @@
+package com.zhuwj.common.base;
+
+/**
+ * 基础枚举类
+ */
+public interface BaseEnum {
+    /**
+     * code
+     *
+     * @return
+     */
+    Integer getCode();
+
+    /**
+     * 显示值
+     *
+     * @return
+     */
+    String getLabel();
+}
--- lazy-common/src/main/java/com/zhuwj/common/exception/BusinessException.java
+++ lazy-common/src/main/java/com/zhuwj/common/exception/BusinessException.java
@@ -2,6 +2,8 @@ package com.zhuwj.common.exception;
 
 
 import com.zhuwj.common.enums.ErrorCodeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /**
  * Description: 业务异常定义
@@ -9,6 +11,8 @@ import com.zhuwj.common.enums.ErrorCodeEnum;
  * author     zhuwj
  * version    V1.0
  */
+@AllArgsConstructor
+@Data
 public class BusinessException extends RuntimeException {
 
     /**
@@ -27,62 +31,14 @@ public class BusinessException extends RuntimeException {
     private Exception exception;
 
 
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Exception getException() {
-        return exception;
-    }
-
-    public void setException(Exception exception) {
-        this.exception = exception;
-    }
-
-    /**
-     *
-     * @param code 错误码
-     * @param message 错误消息
-     * @param exception 异常
-     */
-    public BusinessException(String code, String message, Exception exception) {
-        this.code = code;
-        this.message = message;
-        this.exception = exception;
-    }
-
-    /**
-     *
-     * @param code  错误码
-     * @param message 错误消息
-     */
-    public BusinessException(String code, String message) {
-        this.code = code;
-        this.message = message;
-    }
-
-
     /**
      * 错误枚举类
+     *
      * @param codeEnum
      */
     public BusinessException(ErrorCodeEnum codeEnum) {
         this.code = codeEnum.getCode();
         this.message = codeEnum.getMsg();
     }
-    public BusinessException() {
-    }
+
 }
